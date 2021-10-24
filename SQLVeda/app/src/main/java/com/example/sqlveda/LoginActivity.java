89fc51a65ab96bc0c15a6bcaa0dbb2e57984a06e
Veda Nadira KAnz Luay <vedanadirakanzluay@gmail.com>
2021-10-24 21:14:28 +0700

    Create LoginActivity.java

---
+++ SQLVeda/app/src/main/java/com/example/sqlveda/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.sqlveda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.sqlveda.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button btn_login1;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.username1);
+        password = (EditText) findViewById(R.id.password1);
+        btn_login1 = (Button) findViewById(R.id.btn_login1);
+        DB = new DBHelper(this);
+
+        btn_login1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals("")){
+                    Toast.makeText(LoginActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if (checkuserpass == true){
+                        Toast.makeText(LoginActivity.this, "Sign In successfull", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Invalid!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+}
